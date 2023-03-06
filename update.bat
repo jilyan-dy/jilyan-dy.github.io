bb0b4638fd673ccf904aa372531e46a7e110530f
snowy-night <jilyan_dy@dlsu.edu.ph>
2023-03-06 16:50:58 +0800

    fixed bat file

--- update.bat
+++ update.bat
@@ -1,19 +1,19 @@
 @echo off
 set message=%1
 
-@REM gatsby build
+gatsby build
 git add -A
 git commit -m %message%
 git push origin main
 git status
 
-@REM cd public
-@REM move * D:\Users\jilyan\React\jilyan-portfolio-baked
+cd public
+move * D:\Users\jilyan\React\jilyan-portfolio-baked
 
-@REM cd D:\Users\jilyan\React\jilyan-portfolio-baked
-@REM git add -A
-@REM git commit -m "commit message"
-@REM git push origin gh-pages
-@REM git status
+cd D:\Users\jilyan\React\jilyan-portfolio-baked
+git add -A
+git commit -m %message%
+git push origin gh-pages
+git status
 
-@REM cd D:\Users\jilyan\React
+cd D:\Users\jilyan\React
