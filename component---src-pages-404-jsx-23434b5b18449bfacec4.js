cb5f9c6529a23dcbac0a9fd2fcb049d8c0726aa6
snowy-night <jilyan_dy@dlsu.edu.ph>
2023-03-17 15:08:48 +0800

    fixed projects attempt 2

--- component---src-pages-404-jsx-23434b5b18449bfacec4.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[256],{7291:function(e,t,r){r.r(t),r.d(t,{default:function(){return s}});var l=r(7294),n=r(6402);function s(){return l.createElement(n.Z,null,l.createElement("div",{className:"doesNotExist"},l.createElement("h2",null,"Sorry, Page Does Not Exist...")))}}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-23434b5b18449bfacec4.js.map
