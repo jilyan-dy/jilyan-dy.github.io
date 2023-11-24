ffbd913f2b7f687aa440f3f9b4f3d3e123f8deb0
snowy-night <jilyan_dy@dlsu.edu.ph>
2023-11-24 13:56:52 +0800

    updated resume

--- update.bat
+++ update.bat
@@ -1,16 +1,20 @@
-@echo off
+@REM @echo off
 set message=%1
 
-@REM gatsby build
+call gatsby clean
+call gatsby build
 git add -A
 git commit -m %message%
 git push origin main
 git status
 
-cd public
-move * D:\Users\jilyan\React\jilyan-portfolio-baked
-
 cd D:\Users\jilyan\React\jilyan-portfolio-baked
+
+for /d %%a IN (*.*) DO IF /i NOT "%%~nxa"=="CNAME" RD /S /Q "%%a"
+for %%i in (*.*) do if not "%%i"=="CNAME" del /q "%%i"
+
+robocopy D:\Users\jilyan\React\jilyan-portfolio\public D:\Users\jilyan\React\jilyan-portfolio-baked /E
+
 git add -A
 git commit -m %message%
 git push origin gh-pages
